6255dbe5ec3204a9fd00e79af3851c6f4f3853a8
nzupec <nzupec@nil.si>
2014-10-25 16:11:47 +0200

    Bootstrap theme integration.

--- meetup_basket/urls.py
+++ meetup_basket/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from dashboard.views import DashboardView
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'meetup_basket.views.home', name='home'),
+    url(r'^$', DashboardView.as_view(), name='dashboard'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
