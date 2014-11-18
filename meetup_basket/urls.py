d551d764c9007748d239a4013ba484c652c2cfb9
Nejc Zupec <nzupec@nil.si>
2014-11-18 22:20:39 +0100

    Order of menu entries.

--- meetup_basket/urls.py
+++ meetup_basket/urls.py
@@ -7,11 +7,11 @@ from web.views import DashboardView, MembersView, MeetupsView, TeamGeneratorView
 
 
 urlpatterns = patterns('',
-    url(r'^$', DashboardView.as_view(), name='dashboard'),
+    url(r'^dashboard/$', DashboardView.as_view(), name='dashboard'),
+    url(r'^$', MembersView.as_view(), name='members'),
     url(r'^meetups/$', MeetupsView.as_view(), name='meetups'),
-    url(r'^members/$', MembersView.as_view(), name='members'),
-    url(r'^team-generator/$', TeamGeneratorView.as_view(), name='team_generator'),
     url(r'^payments/$', PaymentsView.as_view(), name='payments'),
+    url(r'^team-generator/$', TeamGeneratorView.as_view(), name='team_generator'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
