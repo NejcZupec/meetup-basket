2863ee6fe87358625d81818b19bc8a7614846e02
nzupec <nzupec@nil.si>
2015-09-14 22:00:09 +0200

    Setup local PostgreSQL database.

--- meetup_basket/settings/base.py
+++ meetup_basket/settings/base.py
@@ -74,8 +74,10 @@ WSGI_APPLICATION = 'meetup_basket.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'meetupbasket',
+        'USER': 'meetupbasket',
+        'HOST': '',
     }
 }
 
