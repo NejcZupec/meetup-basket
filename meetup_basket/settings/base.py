cac72877351de936e7895acbafc6a1c92788ece7
nzupec <nzupec@nil.si>
2015-10-13 20:27:26 +0200

    Turn off debug mode in production.

--- meetup_basket/settings/base.py
+++ meetup_basket/settings/base.py
@@ -38,7 +38,7 @@ SECRET_KEY = get_env_variable("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = ['*']
 
--- meetup_basket/settings/production.py
+++ meetup_basket/settings/production.py
@@ -1,5 +1,8 @@
 from base import *
 
+
+DEBUG = False
+
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 DATABASES = {
